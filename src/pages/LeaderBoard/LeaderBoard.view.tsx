d36578949029df56b1442404a47e5edc8d98f431
Roolman <roman-savelev4@yandex.ru>
2021-12-22 17:52:10 +0300

    mock папка добавлена. верстка leaderboard сделана

--- src/pages/LeaderBoard/LeaderBoard.view.tsx
+++ src/pages/LeaderBoard/LeaderBoard.view.tsx
@@ -2,91 +2,31 @@ import Header from "@/components/Header";
 import Container from "@/components/Container";
 import React, { FC } from "react";
 import {Table} from "antd"
-import styles from "./LeaderBoard.module.scss"
+import { ColumnsType } from 'antd/es/table';
 
-const columns = [
+import styles from "./LeaderBoard.module.scss"
+import { LeaderBoardData, LeaderBoardUser } from "@/mock/leaderboard";
+
+const columns: ColumnsType<LeaderBoardUser> = [
     {
-      title: 'Name',
+      title: 'N',
+      dataIndex: 'position',
+    },
+    {
+      title: 'Имя',
       dataIndex: 'name',
-      filters: [
-        {
-          text: 'Joe',
-          value: 'Joe',
-        },
-        {
-          text: 'Jim',
-          value: 'Jim',
-        },
-        {
-          text: 'Submenu',
-          value: 'Submenu',
-          children: [
-            {
-              text: 'Green',
-              value: 'Green',
-            },
-            {
-              text: 'Black',
-              value: 'Black',
-            },
-          ],
-        },
-      ],
-      // specify the condition of filtering result
-      // here is that finding the name started with `value`
-      onFilter: (value: any, record: any) => record.name.indexOf(value) === 0,
-      sorter: (a: any, b: any) => a.name.length - b.name.length,
+      sorter: (a: LeaderBoardUser, b: LeaderBoardUser) => a.name.length - b.name.length,
       sortDirections: ['descend'],
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
+      title: 'Очки',
+      dataIndex: 'score',
       defaultSortOrder: 'descend',
-      sorter: (a: any, b: any) => a.age - b.age,
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      filters: [
-        {
-          text: 'London',
-          value: 'London',
-        },
-        {
-          text: 'New York',
-          value: 'New York',
-        },
-      ],
-      onFilter: (value: any, record: any) => record.address.indexOf(value) === 0,
+      sorter: (a: LeaderBoardUser, b: LeaderBoardUser) => a.score - b.score,
     },
 ];
   
-const data = [
-{
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-},
-{
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-},
-{
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-},
-{
-    key: '4',
-    name: 'Jim Red',
-    age: 32,
-    address: 'London No. 2 Lake Park',
-},
-];
+
   
 function onChange(pagination: any, filters: any, sorter: any, extra: any) {
 console.log('params', pagination, filters, sorter, extra);
@@ -101,7 +41,7 @@ const LeaderBoard: FC<{}> = () => {
                 <Table 
                     className={styles.leaderBoardTable} 
                     rowClassName={styles.leaderBoardTableRow}
-                    columns={columns as any} dataSource={data} 
+                    columns={columns} dataSource={LeaderBoardData} 
                     onChange={onChange} 
                 />
             </div>

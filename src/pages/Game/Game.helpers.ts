fe308bfd84adcb5abc1973698a7898c7597e0c6a
mikhailbys <bystrovmikhailad@gmail.com>
2021-12-23 22:31:22 +0400

    хук-хелпер

---
+++ src/pages/Game/Game.helpers.ts
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+import {useHistory} from "react-router-dom";
+
+import {World} from "@/game/world/world";
+import {routes} from "@/config/routes/routes";
+
+export const useGame = () => {
+    const [active, setActive] = useState(true);
+    const [pause, setPause] = useState(false);
+    const [world] = useState(new World());
+    const canvasRef = React.useRef<HTMLCanvasElement>(null);
+    const history = useHistory();
+
+    const callMenu = (e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            setActive(true);
+            setPause(true);
+            world.setPause(true);
+        }
+    }
+
+    const setUpPauseButton = () => {
+        document.addEventListener('keydown', callMenu);
+    }
+
+    const onClose = () => {
+        setActive(false);
+        world.destroy()
+        history.push(`/#${routes.main.path}`);
+    };
+
+    const onStart = () => {
+        world.init(canvasRef.current);
+        setPause(false);
+        setActive(false);
+    }
+
+    const onResume = () => {
+        world.setPause(false);
+        setPause(false);
+        setActive(false);
+    }
+
+    return {
+        active,
+        pause,
+        canvasRef,
+        onStart,
+        onResume,
+        onClose,
+        setUpPauseButton
+    }
+}
--- src/pages/Game/Game.view.tsx
+++ src/pages/Game/Game.view.tsx
@@ -1,48 +1,16 @@
-import React, {FC, useEffect, useState} from "react";
-import { useHistory } from "react-router-dom";
+import React, {FC, useEffect} from "react";
 
-import {World} from "@/game/world/world";
-import {routes} from '@/config/routes/routes';
+import {useGame} from "@/pages/Game/Game.helpers";
 
 import Menu from './Menu';
 import styles from './Game.module.scss';
 
 const Game: FC = () => {
-    const [active, setActive] = useState(true);
-    const [pause, setPause] = useState(false);
-    const [world] = useState(new World());
-    const canvasRef = React.useRef<HTMLCanvasElement>(null);
-    const history = useHistory();
-
-    const callMenu = (e: KeyboardEvent) => {
-        if (e.key === 'Escape') {
-            setActive(true);
-            setPause(true);
-            world.setPause(true);
-        }
-    }
+   const { active, pause, canvasRef, onStart, onResume, onClose, setUpPauseButton } = useGame();
 
     useEffect(() => {
-        document.addEventListener('keydown', callMenu);
+        setUpPauseButton();
     }, []);
-
-    const onClose = () => {
-        setActive(false);
-        world.destroy()
-        history.push(`/#${routes.main.path}`);
-    };
-
-    const onStart = () => {
-        world.init(canvasRef.current);
-        setPause(false);
-        setActive(false);
-    }
-
-    const onResume = () => {
-        world.setPause(false);
-        setPause(false);
-        setActive(false);
-    }
 
     return (
         <>

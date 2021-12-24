8a782d0d3df1053c5b971ed7a7b6d0ba75b9c774
Roman Savelev <romansavelev@MacBook-Air-Roman.local>
2021-12-24 16:02:22 +0300

    Удалены компоненты, повторяющие antd. Глобальные стили вынесены в отдельные файлы

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App.view";
 
+import "./styles/button.scss";
+import "./styles/input.scss";
+
 ReactDOM.render(<App/>, document.getElementById("root"));

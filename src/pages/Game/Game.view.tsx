7909cc2ad5c880673306d48ddcba4e61bca95f4d
Dyomin Ilya <alkhipce@mail.ru>
2021-12-26 06:05:28 +0600

    feat: реализовать модалку гамовер

--- src/pages/Game/Game.view.tsx
+++ src/pages/Game/Game.view.tsx
@@ -1,39 +1,52 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 
-import {useGame} from "@/pages/Game/Game.helpers";
+import { useGame } from "@/pages/Game/Game.helpers";
 
-import Menu from './Menu';
-import styles from './Game.module.scss';
+import Menu from "./Menu";
+import styles from "./Game.module.scss";
 import { useMountEffect } from "@/hooks/useMountEffect";
 import { useUnmountEffect } from "@/hooks/useUnmountEffect";
 
+let callGameOverOutside = () => {};
+
 const Game: FC = () => {
-   const {
-       isActive, isPaused, canvasRef, 
-       onStart, onResume, onClose, 
-       onUnmount, setUpPauseButton 
-    } = useGame();
+  const {
+    isActive,
+    isPaused,
+    canvasRef,
+    onStart,
+    onResume,
+    onClose,
+    onUnmount,
+    setUpPauseButton,
+    callGameOver,
+    isGameOver,
+  } = useGame();
 
-    useMountEffect(() => {
-        setUpPauseButton()
-    })
+  callGameOverOutside = callGameOver;
 
-    useUnmountEffect(() => {
-        onUnmount()
-    })
+  useMountEffect(() => {
+    setUpPauseButton();
+  });
 
-    return (
-        <>
-            <canvas className={styles.game} ref={canvasRef} />
-            <Menu
-                isActive={isActive}
-                onClose={onClose}
-                onStart={onStart}
-                isPaused={isPaused}
-                onResume={onResume}
-            />
-        </>
-    )
+  useUnmountEffect(() => {
+    onUnmount();
+  });
+
+  return (
+    <>
+      <canvas className={styles.game} ref={canvasRef} />
+      <Menu
+        isActive={isActive}
+        onClose={onClose}
+        onStart={onStart}
+        isPaused={isPaused}
+        isGameOver={isGameOver}
+        onResume={onResume}
+      />
+    </>
+  );
 };
-  
-export default Game
+
+export { callGameOverOutside };
+export default Game;

356c52954cba221377608976c7081aeb0e33fb6d
Dyomin Ilya <alkhipce@mail.ru>
2021-12-26 06:05:28 +0600

    feat: реализовать модалку гамовер

--- src/game/world/objects/enemy.ts
+++ src/game/world/objects/enemy.ts
@@ -1,34 +1,29 @@
 import { Object2D, Object2DProps } from "../../../game/core/object";
-import { Collidable } from "../../core/physics/physics"
-import { Player } from "./player"
+import { Collidable } from "../../core/physics/physics";
+import { Player } from "./player";
 
-type EnemyProps = Object2DProps & {
-}
+import { callGameOverOutside } from "@/pages/Game/Game.view";
+
+type EnemyProps = Object2DProps & {};
 
 export class Enemy extends Object2D implements Collidable {
+  canCollide: boolean = true;
 
-    canCollide: boolean = true
+  constructor(props: EnemyProps) {
+    super(props);
 
-    constructor(props: EnemyProps) {
-        super(props)
-        
-        this.init()
+    this.init();
+  }
+
+  init() {}
+
+  updateState(dt: number) {}
+
+  //
+  onCollide(obstacle: Object2D & Collidable) {
+    if (obstacle instanceof Player) {
+      callGameOverOutside();
+      console.log("Game over !");
     }
-
-    init() {
-       
-    }
-
-    updateState(dt: number) {
-
-    }
-
-
-    // 
-    onCollide(obstacle: Object2D & Collidable) {
-        if(obstacle instanceof Player) {
-            console.log("Game over !")
-        }
-    }
-
+  }
 }
--- src/game/world/world.ts
+++ src/game/world/world.ts
@@ -1,185 +1,185 @@
-import { Camera } from "../core/camera"
-import { RectangleGeometry } from "../core/geometry/rectangle/rectangle"
-import {Renderer} from "../core/renderer"
-import { Scene } from "../core/scene"
-import { EventBus, EventTypes, KeyboardEvents } from "../core/eventBus"
-import { Color } from "../core/utils/color"
-import { Player } from "./objects/player"
-import { Enemy } from "./objects/enemy"
-import { Wall } from "./objects/wall"
+import { Camera } from "../core/camera";
+import { RectangleGeometry } from "../core/geometry/rectangle/rectangle";
+import { Renderer } from "../core/renderer";
+import { Scene } from "../core/scene";
+import { EventBus, EventTypes, KeyboardEvents } from "../core/eventBus";
+import { Color } from "../core/utils/color";
+import { Player } from "./objects/player";
+import { Enemy } from "./objects/enemy";
+import { Wall } from "./objects/wall";
 
-type Listener = (this: Window, ev: KeyboardEvent) => any
+type Listener = (this: Window, ev: KeyboardEvent) => any;
 
 // Игровой мир
 export class World {
+  canvas: HTMLCanvasElement | null;
+  renderer: Renderer;
+  scene: Scene;
+  camera: Camera;
 
-    canvas: HTMLCanvasElement | null
-    renderer: Renderer
-    scene: Scene
-    camera: Camera
+  eventBus: EventBus;
+  pause: boolean = false;
 
-    eventBus: EventBus
-    pause: boolean = false
+  // События, от которых нужно отписаться
+  private _keyDownListener: Listener;
+  private _keyUpListener: Listener;
 
-    // События, от которых нужно отписаться
-    private _keyDownListener: Listener
-    private _keyUpListener: Listener
+  constructor() {}
 
-    constructor() {
+  init(canvas: HTMLCanvasElement | null) {
+    this.canvas = canvas;
+    this.eventBus = new EventBus();
 
-    }
+    // Создаем Renderer
+    this.renderer = new Renderer({
+      canvas: canvas,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
 
-    init(canvas: HTMLCanvasElement | null) {
-        
-        this.canvas = canvas
-        this.eventBus = new EventBus()
-        
-        // Создаем Renderer
-        this.renderer = new Renderer({
-            canvas: canvas,
-            width: window.innerWidth,
-            height: window.innerHeight
-        })
+    // Задаем бэкграунд и создаем сцену
+    const background = new Color(0, 0, 255);
+    this.scene = new Scene(background);
+    // Создаем камеру (пока что пустую)
+    // const camera = new Camera()
 
-        // Задаем бэкграунд и создаем сцену
-        const background = new Color(0, 0, 255)
-        this.scene = new Scene(background)
-        // Создаем камеру (пока что пустую)
-        // const camera = new Camera()
+    // Создаем Игрока
+    const playerGeom = new RectangleGeometry(36, 36);
+    const player = new Player({
+      geometry: playerGeom,
+      eventBus: this.eventBus,
+    });
+    // Зададим дефолтное положение
+    player.positon.x = window.innerWidth / 2;
+    player.positon.y = window.innerHeight / 2;
+    // Загружаем изображение для спрайта игрока
+    const image = new Image(36, 36);
+    image.src = "player.png";
+    // Устанавливаем спрайт
+    player.sprite = image;
 
-        // Создаем Игрока
-        const playerGeom = new RectangleGeometry(36, 36)
-        const player = new Player({
-            geometry: playerGeom,
-            eventBus: this.eventBus
-        })
-        // Зададим дефолтное положение        
-        player.positon.x = window.innerWidth / 2
-        player.positon.y = window.innerHeight / 2
-        // Загружаем изображение для спрайта игрока
-        const image = new Image(36, 36)
-        image.src = "player.png"
-        // Устанавливаем спрайт
-        player.sprite = image
+    // Создаем противника
+    const enemyGeom = new RectangleGeometry(36, 36);
+    const enemy = new Enemy({
+      geometry: enemyGeom,
+      color: new Color(255, 0, 0),
+    });
+    // Зададим дефолтное положение
+    enemy.positon.x = window.innerWidth / 4;
+    enemy.positon.y = window.innerHeight / 4;
 
-        // Создаем противника
-        const enemyGeom = new RectangleGeometry(36, 36)
-        const enemy = new Enemy({
-            geometry: enemyGeom,
-            color: new Color(255, 0, 0)
-        })
-        // Зададим дефолтное положение        
-        enemy.positon.x = window.innerWidth / 4
-        enemy.positon.y = window.innerHeight / 4
+    // Создаем стены
+    const wall1Geom = new RectangleGeometry(30, 400);
+    const wall1 = new Wall({
+      geometry: wall1Geom,
+      color: new Color(0, 255, 0),
+    });
+    wall1.positon.x = window.innerWidth / 2 - 200;
+    wall1.positon.y = window.innerHeight / 4;
 
-        // Создаем стены
-        const wall1Geom = new RectangleGeometry(30, 400)
-        const wall1 = new Wall({
-            geometry: wall1Geom,
-            color: new Color(0, 255, 0)
-        })
-        wall1.positon.x = window.innerWidth / 2 - 200
-        wall1.positon.y = window.innerHeight / 4
+    const wall2Geom = new RectangleGeometry(30, 400);
+    const wall2 = new Wall({
+      geometry: wall2Geom,
+      color: new Color(0, 255, 0),
+    });
+    wall2.positon.x = window.innerWidth / 2 + 200;
+    wall2.positon.y = window.innerHeight / 4;
 
-        const wall2Geom = new RectangleGeometry(30, 400)
-        const wall2 = new Wall({
-            geometry: wall2Geom,
-            color: new Color(0, 255, 0)
-        })
-        wall2.positon.x = window.innerWidth / 2 + 200
-        wall2.positon.y = window.innerHeight / 4
+    const wall3Geom = new RectangleGeometry(400, 30);
+    const wall3 = new Wall({
+      geometry: wall3Geom,
+      color: new Color(0, 255, 0),
+    });
+    wall3.positon.x = window.innerWidth / 2 - 175;
+    wall3.positon.y = window.innerHeight / 2 - 290;
 
-        const wall3Geom = new RectangleGeometry(400, 30)
-        const wall3 = new Wall({
-            geometry: wall3Geom,
-            color: new Color(0, 255, 0)
-        })
-        wall3.positon.x = window.innerWidth / 2 - 175
-        wall3.positon.y = window.innerHeight / 2 - 290
+    // NOTE: Порядок подключения влияет на очередь отрисовки
+    this.scene.add(wall1);
+    this.scene.add(wall2);
+    this.scene.add(wall3);
+    this.scene.add(player);
+    this.scene.add(enemy);
 
-        // NOTE: Порядок подключения влияет на очередь отрисовки
-        this.scene.add(wall1)
-        this.scene.add(wall2)
-        this.scene.add(wall3)
-        this.scene.add(player)
-        this.scene.add(enemy)
+    this.registerEvents();
+    this.animate();
+  }
 
-        this.registerEvents()
-        this.animate()
-    }
+  setPause(pause: boolean) {
+    this.pause = pause;
+  }
 
-    setPause(pause: boolean) {
-        this.pause = pause;
-    }
+  reinit() {
+    this.init(this.canvas);
+  }
 
-    animate() {
-        const renderer = this.renderer as Renderer
-        const scene = this.scene as Scene
-        const camera = this.camera as Camera
+  animate() {
+    const renderer = this.renderer as Renderer;
+    const scene = this.scene as Scene;
+    const camera = this.camera as Camera;
 
-        let last = performance.now()
-        const render = () => {
-            let now = performance.now()
-            let dt = now - last
-            last = now
-            if (!this.pause) {
-                renderer.prerender(scene, dt, now)
-                // TODO: Реализовать класс camera
-                // camera.update()
-                renderer.render(scene, camera)
-            }
-            requestAnimationFrame(() => render())
-        }
+    let last = performance.now();
+    const render = () => {
+      let now = performance.now();
+      let dt = now - last;
+      last = now;
 
-        render()
-    }
+      if (!this.pause) {
+        renderer.prerender(scene, dt, now);
+        // TODO: Реализовать класс camera
+        // camera.update()
+        renderer.render(scene, camera);
+      }
+      requestAnimationFrame(() => render());
+    };
 
-    registerEvents() {
-        const eventBus = this.eventBus as EventBus
+    render();
+  }
 
-        this._keyDownListener = (e: KeyboardEvent) => {
-            switch(e.key) {
-                case KeyboardEvents.ArrowDown:
-                    eventBus.emit(EventTypes.ArrowBottomDown)
-                    break
-                case KeyboardEvents.ArrowLeft:
-                    eventBus.emit(EventTypes.ArrowLeftDown)
-                    break
-                case KeyboardEvents.ArrowRight:
-                    eventBus.emit(EventTypes.ArrowRightDown)
-                    break
-                case KeyboardEvents.ArrowUp:
-                    eventBus.emit(EventTypes.ArrowTopDown)   
-                default:
-                    break                                                         
-            }
-        }
+  registerEvents() {
+    const eventBus = this.eventBus as EventBus;
 
-        this._keyUpListener = (e: KeyboardEvent) => {
-            switch(e.key) {
-                case KeyboardEvents.ArrowDown:
-                    eventBus.emit(EventTypes.ArrowBottomUp)
-                    break
-                case KeyboardEvents.ArrowLeft:
-                    eventBus.emit(EventTypes.ArrowLeftUp)
-                    break
-                case KeyboardEvents.ArrowRight:
-                    eventBus.emit(EventTypes.ArrowRightUp)
-                    break
-                case KeyboardEvents.ArrowUp:
-                    eventBus.emit(EventTypes.ArrowTopUp)     
-                default:
-                    break                                                       
-            }            
-        }
+    this._keyDownListener = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case KeyboardEvents.ArrowDown:
+          eventBus.emit(EventTypes.ArrowBottomDown);
+          break;
+        case KeyboardEvents.ArrowLeft:
+          eventBus.emit(EventTypes.ArrowLeftDown);
+          break;
+        case KeyboardEvents.ArrowRight:
+          eventBus.emit(EventTypes.ArrowRightDown);
+          break;
+        case KeyboardEvents.ArrowUp:
+          eventBus.emit(EventTypes.ArrowTopDown);
+        default:
+          break;
+      }
+    };
 
-        window.addEventListener("keydown", this._keyDownListener)
-        window.addEventListener("keyup", this._keyUpListener)
-    }
+    this._keyUpListener = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case KeyboardEvents.ArrowDown:
+          eventBus.emit(EventTypes.ArrowBottomUp);
+          break;
+        case KeyboardEvents.ArrowLeft:
+          eventBus.emit(EventTypes.ArrowLeftUp);
+          break;
+        case KeyboardEvents.ArrowRight:
+          eventBus.emit(EventTypes.ArrowRightUp);
+          break;
+        case KeyboardEvents.ArrowUp:
+          eventBus.emit(EventTypes.ArrowTopUp);
+        default:
+          break;
+      }
+    };
 
-    destroy() {
-        window.removeEventListener("keydown", this._keyDownListener)
-        window.removeEventListener("keyup", this._keyUpListener)
-    }
+    window.addEventListener("keydown", this._keyDownListener);
+    window.addEventListener("keyup", this._keyUpListener);
+  }
 
+  destroy() {
+    window.removeEventListener("keydown", this._keyDownListener);
+    window.removeEventListener("keyup", this._keyUpListener);
+  }
 }

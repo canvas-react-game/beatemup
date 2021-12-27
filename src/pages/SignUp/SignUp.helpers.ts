fd8b91db6b7648710593a5db08961e82567de48e
mikhailbys <bystrovmikhailad@gmail.com>
2021-12-27 22:49:39 +0400

    правки

--- src/pages/SignUp/SignUp.helpers.ts
+++ src/pages/SignUp/SignUp.helpers.ts
@@ -17,13 +17,35 @@ export const useSignUp = () => {
     const currentPath = routes.signUp.path;
 
     const fieldSet = [
-        {name: SignUpFieldNames.SecondName, rules: [{ required: true, message: 'Введите фамилию' }], placeholder: 'Фамилия'},
-        {name: SignUpFieldNames.FirstName, rules: [{ required: true, message: 'Введите имя' }], placeholder: 'Имя'},
-        {name: SignUpFieldNames.Email, rules: [{ required: true, message: 'Введите почту' }], placeholder: 'Эл. почта'},
-        {name: SignUpFieldNames.Phone, rules: [{ required: true, message: 'Введите телефон' }], placeholder: 'Телефон'},
-        {name: SignUpFieldNames.Login, rules: [{ required: true, message: 'Введите логин' }], placeholder: 'Логин'},
         {
-            name: SignUpFieldNames.Password, rules: [{ required: true, message: 'Введите пароль' }], placeholder: 'Пароль',
+            name: SignUpFieldNames.SecondName,
+            rules: [{ required: true, message: 'Введите фамилию' }],
+            placeholder: 'Фамилия'
+        },
+        {
+            name: SignUpFieldNames.FirstName,
+            rules: [{ required: true, message: 'Введите имя' }],
+            placeholder: 'Имя'
+        },
+        {
+            name: SignUpFieldNames.Email,
+            rules: [{ required: true, message: 'Введите почту' }],
+            placeholder: 'Эл. почта'
+        },
+        {
+            name: SignUpFieldNames.Phone,
+            rules: [{ required: true, message: 'Введите телефон' }],
+            placeholder: 'Телефон'
+        },
+        {
+            name: SignUpFieldNames.Login,
+            rules: [{ required: true, message: 'Введите логин' }],
+            placeholder: 'Логин'
+        },
+        {
+            name: SignUpFieldNames.Password,
+            rules: [{ required: true, message: 'Введите пароль' }],
+            placeholder: 'Пароль',
             type:'password'
         },
     ]

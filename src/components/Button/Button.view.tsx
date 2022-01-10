31d01f8e03d531460a2bb3f51f9abd88944430b6
Roolman <roman-savelev4@yandex.ru>
2022-01-10 15:12:31 +0300

    lint auto commit

--- src/components/Button/Button.view.tsx
+++ src/components/Button/Button.view.tsx
@@ -1,11 +1,10 @@
-import React, {FC} from "react";
-import { Button as CustomButton, ButtonProps } from 'antd';
+import React, { FC } from "react";
+import { Button as CustomButton, ButtonProps } from "antd";
 
-import styles from './Button.module.scss';
+import styles from "./Button.module.scss";
 
 interface Props extends ButtonProps {}
 
-const Button:FC<Props> = (props) =>
-    (<CustomButton className={styles.antBtnPrimary}  {...props}/>);
+const Button:FC<Props> = (props) => (<CustomButton className={styles.antBtnPrimary} {...props}/>);
 
 export default Button;

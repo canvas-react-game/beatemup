751f82a6ca19580dbdcf54a71c84caff6bd496fe
Roman Savelev <romansavelev@MacBook-Air-Roman.local>
2022-01-10 11:57:20 +0300

    eslint fix

--- src/pages/Game/Menu/Menu.view.tsx
+++ src/pages/Game/Menu/Menu.view.tsx
@@ -1,51 +1,49 @@
 import React, { FC } from "react";
-import { Modal as CustomModal, Typography } from "antd";
-
-import Button from "@/components/Button";
+import { Modal as CustomModal, Typography, Button } from "antd";
 
 import styles from "./Main.module.scss";
 
 const { Title } = Typography;
 interface Props {
-  isActive: boolean;
-  isPaused: boolean;
-  isGameOver: boolean;
-  onClose: () => void;
-  onStart: () => void;
-  onResume: () => void;
+    isActive: boolean;
+    isPaused: boolean;
+    isGameOver: boolean;
+    onClose: () => void;
+    onStart: () => void;
+    onResume: () => void;
 }
 
 const Menu: FC<Props> = ({
-  isActive,
-  isPaused,
-  isGameOver,
-  onClose,
-  onStart,
-  onResume,
+    isActive,
+    isPaused,
+    isGameOver,
+    onClose,
+    onStart,
+    onResume,
 }) => (
-  <CustomModal
-    className={styles.gameModal}
-    title="Меню"
-    visible={isActive}
-    footer={null}
-    closable={false}
-  >
-    <div className={styles.buttonContainer}>
-      {isGameOver && (
-        <Title className={styles.title} level={3}>
+    <CustomModal
+        className={styles.gameModal}
+        title="Меню"
+        visible={isActive}
+        footer={null}
+        closable={false}
+    >
+        <div className={styles.buttonContainer}>
+            {isGameOver && (
+                <Title className={styles.title} level={3}>
           Потрачено!
-        </Title>
-      )}
+                </Title>
+            )}
 
-      <Button type="primary" onClick={isPaused ? onResume : onStart}>
-        {isPaused ? "Продолжить" : `Начать ${isGameOver ? "заново" : "игру"}`}
-      </Button>
+            <Button type="primary" onClick={isPaused ? onResume : onStart}>
+                {isPaused ? "Продолжить" : `Начать ${isGameOver ? "заново" : "игру"}`}
+            </Button>
 
-      <Button type="primary" onClick={onClose}>
+            <Button type="primary" onClick={onClose}>
         Вернуться на главную
-      </Button>
-    </div>
-  </CustomModal>
+            </Button>
+        </div>
+    </CustomModal>
 );
 
 export default Menu;

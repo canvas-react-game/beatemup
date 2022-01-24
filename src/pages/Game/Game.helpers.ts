f427610adb2f7bfd67261e7e05e7e5e146924259
mikhailbys <bystrovmikhailad@gmail.com>
2022-01-24 22:47:47 +0400

    фулскрин на окне игры

--- src/pages/Game/Game.helpers.ts
+++ src/pages/Game/Game.helpers.ts
@@ -4,6 +4,14 @@ import { useHistory } from "react-router-dom";
 import { World } from "@/game/world/world";
 import { routes } from "@/config/routes/routes";
 
+const toggleFullScreen = () => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+}
+
 export const useGame = () => {
     const [isActive, setActive] = useState(true);
     const [isGameOver, setGameOver] = useState(false);
@@ -17,6 +25,9 @@ export const useGame = () => {
             setActive(true);
             setPause(true);
             world.stopAnimation();
+        }
+        if (e.key === "f") {
+            toggleFullScreen();
         }
     }, []);
 

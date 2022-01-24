5a592dd0923dad990720526e64f3c5e7d535f596
Roman Savelev <romansavelev@192.168.88.209>
2022-01-25 00:58:40 +0300

    настроены дефолтные тесты

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  clearMocks: true,
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(s?css|less)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src"
+  },
+  roots: [
+    "<rootDir>"
+  ],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+};
+ 

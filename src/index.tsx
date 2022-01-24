0a947b26e079869cf57479d122029b42f942e264
alkhipce <alkhipce@mail.ru>
2022-01-24 23:32:45 +0600

    fix: исправить ошибки в sw и типизировать

--- src/index.tsx
+++ src/index.tsx
@@ -7,23 +7,3 @@ import "./styles/input.scss";
 import "./styles/button.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-function startServiceWorker() {
-    if ("serviceWorker" in navigator) {
-        window.addEventListener("load", () => {
-            navigator.serviceWorker
-                .register("./sw.js")
-                .then((registration) => {
-                    console.log(
-                        "ServiceWorker registration successful ",
-                        registration.scope,
-                    );
-                })
-                .catch((error: string) => {
-                    console.log("ServiceWorker registration failed: ", error);
-                });
-        });
-    }
-}
-
-startServiceWorker();

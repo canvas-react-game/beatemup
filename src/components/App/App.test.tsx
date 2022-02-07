759eb501d62ab0bba198ee25558d3ba76283a4b6
Roman Savelev <romansavelev@MacBook-Air-Roman.local>
2022-02-07 16:36:53 +0300

    App test removed mock for App

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,33 +1,11 @@
 import React from "react";
-import { mount, shallow } from "enzyme";
+import { mount } from "enzyme";
 import App from "@/components/App";
 
-jest.mock("@/components/App", () => function App() {
-    return (
-        <div className="App">
-
-        </div>
-    );
-});
-
 describe("App component", () => {
-    test("mock App renders correctly", () => {
+    test("App renders correctly", () => {
         const tree = mount(<App/>);
 
         expect(tree).toMatchSnapshot();
-    });
-
-    test("mock App should be Truthy", () => {
-        const AppComponent = mount(<App/>);
-        expect(AppComponent).toBeTruthy();
-    });
-
-    test("mock App should have correct className", () => {
-        const APP_CLASSNAME = "App";
-        const AppWrapper = shallow(<App/>);
-
-        const AppComponent = AppWrapper.find("div");
-
-        expect(AppComponent.hasClass(APP_CLASSNAME)).toBe(true);
     });
 });

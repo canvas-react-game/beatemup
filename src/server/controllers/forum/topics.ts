b81cc37f7bce4ef769597b308b73c5dfd6f07bd5
Roman Savelev <romansavelev@MacBook-Air-Roman.local>
2022-03-01 17:49:04 +0300

    Переписана логика на класс + декоратор для ошибок

--- src/server/controllers/forum/topics.ts
+++ src/server/controllers/forum/topics.ts
@@ -1,34 +1,37 @@
 import {Request, Response} from "express"
 import { ValidationError } from "sequelize"
 import { addDBTopic, getDBTopics } from "@/server/db/forum/actions/topics"
+import { createSafeDecorator } from "@/server/utils/safeDecorator"
 
-export const getTopics = async (req: Request, res: Response) => {
-    try {
+const topicErrorHandler = (err: unknown, res: Response) => {
+    if(err instanceof ValidationError) {
+        return res.status(400).send({message: "Неверный формат данных"})
+    } else {
+        return res.status(500).send({message: "Ошибка сервера"})
+    }
+}
+const Safe = createSafeDecorator(topicErrorHandler)
+
+class TopicController {
+
+    @Safe
+    async get(req: Request, res: Response) {
         const topics = await getDBTopics()
         if(!topics || !topics.length) {
             return res.status(400).send({message: "Топики отсутствуют"})
         }
         return res.status(200).send(topics)
     }
-    catch(err) {
-        console.log(err)
-        return res.status(500).send({message: "Ошибка сервера"})
-    }
-}
 
-export const addTopic = async (req: Request, res: Response) => {
-    try {
+    @Safe
+    async add(req: Request, res: Response) {
         const topic = await addDBTopic(req.body)
         if(!topic) {
             return res.status(400).send({message: "Ошибка добавления топика"})
         }
         return res.status(200).send(topic)
     }
-    catch(err) {
-        if(err instanceof ValidationError) {
-            return res.status(400).send({message: "Ошибка добавления топика. Неверный формат данных"})
-        } else {
-            return res.status(500).send({message: "Ошибка сервера"})
-        }
-    }
+
 }
+
+export default new TopicController()

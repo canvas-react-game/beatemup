6e78789ca2e11fb300f68649f063c84192fa927f
Roman Savelev <romansavelev@MacBook-Air-Roman.local>
2022-03-02 13:20:29 +0300

    Закрыты ручки за авторизацией. Проверяем авторизацию только по кукам яндекса

--- src/server/router/yandexApiRoutes.ts
+++ src/server/router/yandexApiRoutes.ts
@@ -3,9 +3,10 @@ import { base } from "@/services/API/API.service";
 import {
     apiProxy,
 } from "../middlewares";
+import AuthService from "@/server/services/auth"
 
 const yandexApiRouter = express.Router();
 
-yandexApiRouter.all(`${base}/*`, apiProxy);
+yandexApiRouter.all(`${base}/*`, apiProxy, AuthService.setAuth.bind(AuthService));
 
 export { yandexApiRouter };

ed9bee63a6261e9da31d276a8185091e8737c159
Roman Savelev <romansavelev@192.168.88.209>
2022-03-08 13:52:40 +0300

    fix auth middleware

--- src/server/router/yandexApiRoutes.ts
+++ src/server/router/yandexApiRoutes.ts
@@ -3,10 +3,10 @@ import { base } from "@/services/API/API.service";
 import {
     apiProxy,
 } from "../middlewares";
-import AuthService from "@/server/services/auth";
+import { checkAuth } from "@/server/middlewares/auth";
 
 const yandexApiRouter = express.Router();
 
-yandexApiRouter.all(`${base}/*`, apiProxy, AuthService.setAuth.bind(AuthService));
+yandexApiRouter.all(`${base}/*`, apiProxy, checkAuth);
 
 export { yandexApiRouter };

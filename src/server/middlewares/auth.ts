e83ebf43ad19f714e09e5c07fe0419ce6c9fa22b
Roman Savelev <romansavelev@192.168.88.209>
2022-03-08 13:54:56 +0300

    lint auto commit

--- src/server/middlewares/auth.ts
+++ src/server/middlewares/auth.ts
@@ -2,13 +2,13 @@ import { Request, Response, NextFunction } from "express";
 import { HttpStatuses } from "@/server/utils/httpStatuses";
 
 export const checkAuth = (
-    req: Request, 
-    res: Response, 
-    next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction,
 ) => {
     if (req.cookies.uuid && req.cookies.authCookie) {
         next();
     } else {
         res.status(HttpStatuses.Unauthorized).send({ message: "Пользователь не авторизован" });
     }
-}
+};

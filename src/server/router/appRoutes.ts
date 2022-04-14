f3e1cce1d2df6547787ce39276e1fe1f7b051905
Roolman <roman-savelev4@yandex.ru>
2022-04-15 01:13:22 +0300

    Переадресация неверных роутов приложения

--- src/server/router/appRoutes.ts
+++ src/server/router/appRoutes.ts
@@ -3,14 +3,14 @@ import { routes, Routes } from "@/config/routes/routes";
 import {
     serverRenderMiddleware,
     limiterMiddleware,
-//    redirectMiddleware,
+    redirectMiddleware,
 } from "../middlewares";
 
 const appRouter = express.Router();
 
 const appRoutes = (function getRoutes(routesMap: Routes): string[] {
     return Object.values(routesMap).reduce<string[]>(
-        (routes, route) => routes.concat([route.path, `${route.path}/*`]),
+        (routes, route) => routes.concat([route.path]),
         [],
     );
 }(routes));
@@ -18,6 +18,6 @@ appRoutes.push("/");
 
 appRouter.get(appRoutes, limiterMiddleware, serverRenderMiddleware);
 // NOTE: Если дошли до сюда, то такого роута не существует и нужно сделать redirect или 404
-// appRouter.get("*", redirectMiddleware);
+appRouter.get("/*", redirectMiddleware);
 
 export { appRouter };

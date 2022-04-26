ac33ce96ac1f7f5778eaacd5aa84cd27f3d359f7
Roolman <roman-savelev4@yandex.ru>
2022-04-26 15:13:34 +0300

    last build

--- dist/wdyr.js
+++ dist/wdyr.js
@@ -1,8 +1,1 @@
-import React from 'react';
-
-if (process.env.NODE_ENV === "development") {
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-  });
-}
+import React from"react";"development"===process.env.NODE_ENV&&require("@welldone-software/why-did-you-render")(React,{trackAllPureComponents:!0});

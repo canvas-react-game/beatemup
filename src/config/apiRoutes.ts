925f25904ebec1dca754c792631bebc596ac2647
Roolman <roman-savelev4@yandex.ru>
2022-04-26 15:10:00 +0300

    OAuth + cookie замена домена на udc.ya-praktikum.tech

--- src/config/apiRoutes.ts
+++ src/config/apiRoutes.ts
@@ -1,9 +1,8 @@
 export const apiRoutes = {
     oauth: {
         providerURLRoot: "https://oauth.yandex.ru/authorize?response_type=code",
-        redirectURI: "https://udg.nomoredomains.work", // localhost:3000
+        redirectURI: "https://udc.ya-praktikum.tech", // localhost:3000
         serviceId: "oauth/yandex/service-id",
         signIn: "oauth/yandex",
-        udcClientId: "b7aa7e3038ed42358ea3cd11acddef45",
     },
 };
